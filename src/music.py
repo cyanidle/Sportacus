2e71c1c182d0d29157c3d7b3e78f4cb1269e7d44
cyanidle <lyosha.doronin@gmail.com>
2022-08-10 22:52:33 +0200

    гыыыы лавалинк

--- src/music.py
+++ src/music.py
@@ -1,12 +1,27 @@
 import asyncio
+from dataclasses import dataclass
 from lavaplayer import Lavalink
 import lightbulb
+from python_redis_lib.settings import Reader
 
+@dataclass
+class MusicPlayerSettings:
+    lavalink_host: str
+    lavalink_port: int
+    lavalink_user_id: int
+    @staticmethod
+    def parse(*args, **kwargs):
+        reader:Reader = kwargs["reader"]
+        return MusicPlayerSettings(**(reader.config_dict.get("music")))
 
 class MusicPlayer:
     def __init__(self) -> None:
+        reader = Reader(file="music.toml")
         ioloop = asyncio.get_running_loop()
-        self.lavalink = Lavalink(loop=ioloop)
+        settings = reader.parse(MusicPlayerSettings)
+        self.lavalink = Lavalink(host = settings.lavalink_host, port = settings.lavalink_port,
+                                 password="youshallnotpass", user_id=settings.lavalink_user_id, loop=ioloop)
+        self.lavalink.connect()
 
     def play(self, ctx:lightbulb.SlashContext):
-        self.lavalink
+        pass
